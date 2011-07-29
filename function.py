62269d4398bd1460f34dc3145322e2c4e1978d28
Bernardo Kyotoku <bernardo@kyotoku.org>
2011-07-29 14:36:20 -0300

    functi

--- function.py
+++ function.py
@@ -1,13 +1,20 @@
-import niScope
+try:
+	import niScope
+except e:
+	print "niScope missing, continuing anyway"
+try:
+	from nidaqmx import AnalogOutputTask
+except e:
+	print "nidaqmx missing, continuing anyway"
 import ordered_symbols
 import logging
 import numpy as np
 import matplotlib.pyplot as plt
 from configobj import ConfigObj
 from validate import Validator
-from nidaqmx import AnalogOutputTask
 from time import sleep
 from multiprocessing import Process, Queue
+from path import *
 
 def log_type(value):
 	try:
@@ -28,18 +35,43 @@ def processor(queue):
 	raw_data = queue.get()
 	rsp_data = reample(raw_data)
 
-def position(point,daq):
-	pass
+def param(config):
+	daq = config['daq']
+	path = daq['path']
+	laser_sweep_frequency = config['laser']['frequency']
+	density = config['image']['density']
+	xlength = path['xf']-path['x0']
+	ylength = path['yf']-path['y0']
+	length = np.sqrt(xlength**2+ylength**2)
+	numRecords = density*length
+	config['scope']['Horizontal']['numRecords'] = numRecords
+	scan_time = numRecords/laser_sweep_frequency
+	x_vel = xlength/scan_time
+	y_vel = ylength/scan_time
+	daq_sample_rate = daq['positioning_time']
+	num_points = daq_sample_rate*daq['positioning']['rate']
+	if mode == 'position':
+		return path['x0'],path['y0'],x_vel,y_vel,scan_time,numPoints
+
+def position(config):
+	path = make_2d_position_path(param(config,'position'))
+	daq = prepare_daq(path,daq_config,'positioning')
+	time.wait_until(daq.task_done)
+	del daq
 
 def park(daq):
 	position([0,0],daq)
 
-def prepare_daq(path,daq_config,mode,auto_start):
+def prepare_daq(path,daq_config,mode,auto_start=True):
+	X_mpV = daq_config['X_mpV']
+	Y_mpV = daq_config['Y_mpV']
+	T = np.array([[X_mpV,Y_mpV]]).T
+	signal = path*T
 	daq = AnalogOutputTask()
 	daq.create_voltage_channel(**daq_config['X'])
 	daq.create_voltage_channel(**daq_config['Y'])
-	daq.configure_timing_sample_clock(**daq_config['positioning'])
-	daq.write(path,auto_start=auto_start)
+	daq.configure_timing_sample_clock(**daq_config[mode])
+	daq.write(signal,auto_start=auto_start)
 	return daq
 
 def prepare_scope(scope_config):
@@ -79,68 +111,15 @@ def allocate_memory(config):
 	data = np.zeros([Z,X,Y],order='C',dtype=np.float64)
 	return data
 
-def line(begin,end,lineDensity):
-	lineDensity = float(lineDensity)
-	v = np.array(end) - np.array(begin)
-	length = np.sqrt(sum(v**2))
-	t = np.linspace(0,1,length*lineDensity)	
-	t.shape += (1,)
-	line = begin + v*t
-	return line.T
-
-def poly3(x1,x2,t1,t2,r1,r2):
-	"""
-	Returns the polynomial coeficients for a curve with the position and 
-	the derivative defined at two points of the curve.
-	"""
-	from numpy.linalg import solve
-	A = np.array([
-	[	3*t1**2,	2*t1, 	1,	0	],
-	[	3*t2**2,	2*t2,	1,	0	],
-	[	t1**3  , 	t1**2,	t1,	1	],
-	[	t2**3  , 	t2**2,	t2,	1	],
-	])
-	B = np.array([r1, r2, x1, x2])
-	return solve(A,B)
-
-def make_scan_path(x0,xf,y0,yf,numRecords,numTomograms):
-	x = np.linspace(x0,xf,numRecords)
-	x.shape = (1,) + x.shape
-	X = x*np.ones((numTomograms,1))
-	y = np.linspace(y0,yf,numTomograms)
-	y.shape = y.shape + (1,)
-	Y = y*np.ones(numRecords)
-	scan_path = np.dstack([X,Y])
-	return scan_path
-
-def make_return_path(x0,xf,y0,yf,tf,r,N,numTomograms):
-	f = np.poly1d(poly3(x0,xf,0,tf,r,r))
-	path = f(np.linspace(0,tf,N))
-	path.shape += (1,)
-	path_x = path*np.ones(numTomograms)
-	path_x = path_x.T
-	y = np.linspace(y0,yf,numTomograms)
-	intervals = [[y[i],y[i+1]] for i in range(numTomograms-1)]+[[y[-1]]*2]
-	path_y = np.vstack([np.linspace(i[0],i[1],N) for i in intervals])
-	p = np.dstack((path_x,path_y))
-	return p
-
-def third_order_line(x1,x2,t1,t2,r1,r2):
-    	f = np.poly1d(poly3(x1,x2,t1,t2,r1,r2))
-	return f(np.arange(t1,t2))
-
-def single_scan_path(X0,Xf,t,lineDensity):
-	pitch = 1/float(lineDensity)
-	num = (Xf-X0)*lineDensity
-	start = third_order_line(0,X0,0,t,0,pitch)
-	scan = np.linspace(X0,Xf,num)
-	park = third_order_line(Xf,0,0,t,pitch,0)
-	return np.hstack([start,scan[0:-1],park])
-    	
 def positioning(daq_task,config):
 	config['daq']['positioning']
 	task.configure_timing_sample_clock(source='OnboardClock', rate=1, active_edge='rising', sample_mode='finite', samples_per_channel=1000)
 	
+def make_line_path(x0,y0,xf,yf,N):
+	X = np.linspace(x0,xf,N)
+	Y = np.linspace(y0,yf,N)
+	return np.vstack((X,Y))
+
 def make_setpositionpath():
 	pass
 
@@ -152,3 +131,128 @@ def make_acquisitionwindowpath():
 
 def make_parkpath():
 	pass
+
+def horz_cal(config,data):
+	scope_config = config['scope']
+	scope = prepare_scope(config['scope'])
+	scope.InitiateAcquisition()
+	num = scope_config['Horizontal']['numPts']
+	rec = scope_config['Horizontal']['numRecords']*2
+	data = np.zeros((num,rec),order='F',dtype=np.float64)
+	scope.Fetch('0,1',data)
+	np.savetxt('ref.dat',data,fmt='%.18e')
+	return data
+
+def load(config,data):
+	data = np.loadtxt('ref.dat').T
+	return data[0]
+
+def non_cor_fft(config,data):
+	return abs(np.fft.fft(data))
+
+def get_p(config,data):
+	data =  y > 0
+	t = np.logical_xor(data,np.roll(data,1))
+	size = len(y)
+	zero_x = np.arange(size)[t]
+	zero_y = np.linspace(0,zero_x[-1],len(zero_x))
+	data = zero_x
+	p = np.polyfit(zero_x,zero_y,7)
+	f = np.poly1d(p)
+	data = np.vstack([zero_y,f(zero_x)]).T
+	for i in p:
+		config['resample_poly_coef']['p%d'%np.where(p==i)] = i
+	config.write()	
+	return data
+
+def scan(config,data):
+	image_config = config['image']
+	lineDensity = image_config['density']
+	length = image_config['length']
+	X = single_scan_path(-length/2,length/2,50,lineDensity)
+	path = np.vstack([X,np.zeros(X.shape)]).T
+	daq = prepare_daq(path,config['daq'],"positioning")
+	scope = prepare_scope(scope_config)
+	scope.NumRecords = path.shape[1]
+	raw_data = scan(scope,daq)
+	rsp_data = resample(raw_data)
+	fft_data = transform(rsp_data)
+	abs_data = abs(fft_data)
+
+def scan_3d(config,data):
+	config3D = config["scope3D"]
+	data = allocate_memory(config3D)
+	scope = prepare_scope(config3D)
+	numTomograms = config3D['numTomograms']
+	conf = config['daq']['positioning']
+	f = config['laser']['frequency']
+	path = config['daq']['path'] 
+	numRec = config3D['Horizontal']['numRecords']
+	tf = float(numRec)/f
+	r = (path['xf']-path['x0'])/tf
+	N = conf['samples_per_channel']
+	x0,xf,y0,yf=path['x0'],path['xf'],path['y0'],path['yf']
+	return_path = make_return_path(x0,xf,y0,yf,tf,r,N,numTomograms)
+	scan_path = make_scan_path(x0,xf,y0,yf,numRec,numTomograms)
+	for i in range(numTomograms):
+		tomogram = data[i]
+		daq = prepare_daq(scan_path[i],config['daq'],"scan3D")
+		scope.InitiateAcquisition()
+		scope.Fetch(config3D['VerticalSample']['channelList'],tomogram)
+		del daq
+		daq = prepare_daq(return_path[i],config['daq'],"positioning",auto_start=True)
+		daq.wait_until_done()
+		del daq
+	# numpy arrays and niscope arrays have weird order, code below fix it
+	new_shape = [data.shape[i] for i in [0,2,1]]
+	data = data.reshape(new_shape)
+	return data
+
+def scan_continuous(config,data):
+	arg = config['daq']['path'].dict()
+	arg['N'] = config['scope']['Horizontal']['numRecords']
+	scan_path = make_line_path(**arg)
+	position(scan_path[0],daq)
+	scope = prepare_scope(config['scope'])
+	while go:
+		daq = prepare_daq(scan_path,config['daq'],'scanContinuous'):
+		scope.InitiateAcquisition()
+		scope.Fetch('0',data)
+		del daq
+		return_mirror(config)
+		data = process(data)
+		plt.imshow(data)
+	park(daq)
+
+def resample(config,data):
+	if len(data.shape) == 3:
+		data = data[0,:,:]
+	return resample(data,config['resample_poly_coef'])
+
+def fft(config,data):
+	data = abs(np.fft.fft(data))
+
+def plot(config,data):
+	import matplotlib.pyplot as plt
+	if len(data.shape) == 3:
+		data = data[0,0,:]
+	if len(data.shape) == 2:
+		if data.shape[1] == 2:
+			plt.plot(data[0],data[1])
+			plt.show()
+			return
+	plt.plot(data)
+	plt.show()
+
+def img_plot(config,data):
+	if len(data.shape) == 3:
+		data = data[0]
+	if len(data.shape) == 2:
+		data = data
+	import matplotlib.pyplot as plt
+	plt.imshow(data)
+	plt.show()
+
+def store(config,data):
+	filename = "data.dat"
+	np.savetxt(filename,data)
--- oct.py
+++ oct.py
@@ -1,160 +1,45 @@
 import ordered_symbols
 import numpy as np
-import matplotlib.pyplot as plt
+import function
+import logging
 from configobj import ConfigObj
 from validate import Validator
-from time import sleep
-from multiprocessing import Process, Queue
-from function import *
 
 def parse():
 	import argparse
 	parser = argparse.ArgumentParser()
 	parser.description = "OCT client."
-	parser.add_argument('--horz-cal',action='store_true')
-	parser.add_argument('--plot',action='store_true')
-	parser.add_argument('--img-plot',action='store_true')
-	parser.add_argument('--store',action='store_true')
-	parser.add_argument('--x',action='store_true')
-	parser.add_argument('--get-p',action='store_true')
-	parser.add_argument('--resample',action='store_true')
-	parser.add_argument('--fft',action='store_true')
-	parser.add_argument('--non-cor-fft',action='store_true')
-	parser.add_argument('--line',action='store_true')
-	parser.add_argument('--scan-3D',action='store_true')
-	parser.add_argument('--scan-continuous',action='store_true')
-	parser.add_argument('--scan',action='store_true')
-	parser.add_argument('--load',action='store_true')
+	flags = [
+		'horz-cal',
+		'plot',
+		'img-plot',
+		'store',
+		'x',
+		'get-p',
+		'resample',
+		'fft',
+		'non-cor-fft',
+		'line',
+		'scan-3D',
+		'scan-continuous',
+		'scan',
+		'load',
+		]
+	for flag in flags:
+		parser.add_argument('--' + flag,action='store_true') 
 	return parser.parse_args()
 
-validator = Validator({'log':log_type,'float':float})
 arg = parse()
+args = filter(lambda element: not element.startswith('_'),dir(arg))
+validator = Validator({'log':function.log_type,'float':float})
 config = ConfigObj('config.ini',configspec='configspec.ini')
 if not config.validate(validator):
 	raise Exception('config.ini does not validate with configspec.ini.')
-import niScope
-from nidaqmx import AnalogOutputTask
 logging.basicConfig(filename='oct.log',level=config['log'])
-scope_config = config['scope']
-if arg.line:
-	print line([1,0],[2,3],10)
 
-if arg.horz_cal:
-	scope = prepare_scope(config['scope'])
-	scope.InitiateAcquisition()
-	num = scope_config['Horizontal']['numPts']
-	rec = scope_config['Horizontal']['numRecords']*2
-	print "Actual record length = ",scope.ActualRecordLength
-	print "Actual sample rate = ",scope.ActualRecordLength
-	data = np.zeros((num,rec),order='F',dtype=np.float64)
-	scope.Fetch('0,1',data)
-	np.savetxt('ref.dat',data,fmt='%.18e')
-	x = None
-			
-if arg.load:
-	data = np.loadtxt('ref.dat').T
-	data = data[0]
-	y = data
+for i in args:
+	if getattr(arg,i):
+		fun = getattr(function,i)
+		data = fun(config,data)	
 
-if arg.non_cor_fft:
-	data = abs(np.fft.fft(data))
 
-if arg.get_p:
-	data =  y > 0
-	t = np.logical_xor(data,np.roll(data,1))
-	size = len(y)
-	zero_x = np.arange(size)[t]
-	zero_y = np.linspace(0,zero_x[-1],len(zero_x))
-	data = zero_x
-	p = np.polyfit(zero_x,zero_y,7)
-	f = np.poly1d(p)
-	data = np.vstack([zero_y,f(zero_x)]).T
-	for i in p:
-		config['resample_poly_coef']['p%d'%np.where(p==i)] = i
-	config.write()	
-	x = zero_x
-
-if arg.scan:
-	image_config = config['image']
-	lineDensity = image_config['density']
-	length = image_config['length']
-	X = single_scan_path(-length/2,length/2,50,lineDensity)
-	path = np.vstack([X,np.zeros(X.shape)]).T
-	daq = prepare_daq(path,config['daq'],"positioning")
-	scope = prepare_scope(scope_config)
-	scope.NumRecords = path.shape[1]
-	raw_data = scan(scope,daq)
-	rsp_data = resample(raw_data)
-	fft_data = transform(rsp_data)
-	abs_data = abs(fft_data)
-
-if arg.scan_3D:
-	config3D = config["scope3D"]
-	data = allocate_memory(config3D)
-	scope = prepare_scope(config3D)
-	numTomograms = config3D['numTomograms']
-	conf = config['daq']['positioning']
-	f = config['laser']['frequency']
-	path = config['daq']['path'] 
-	numRec = config3D['Horizontal']['numRecords']
-	tf = float(numRec)/f
-	import pdb;pdb.set_trace()
-	r = (path['xf']-path['x0'])/tf
-	N = conf['samples_per_channel']
-	x0,xf,y0,yf=path['x0'],path['xf'],path['y0'],path['yf']
-	return_path = make_return_path(x0,xf,y0,yf,tf,r,N,numTomograms)
-	scan_path = make_scan_path(x0,xf,y0,yf,numRec,numTomograms)
-	for i in range(numTomograms):
-		tomogram = data[i]
-		daq = prepare_daq(scan_path[i],config['daq'],"scan3D")
-		scope.InitiateAcquisition()
-		scope.Fetch(config3D['VerticalSample']['channelList'],tomogram)
-		del daq
-		daq = prepare_daq(return_path[i],config['daq'],"positioning",auto_start=True)
-		daq.wait_until_done()
-		del daq
-	# numpy arrays and niscope arrays have weird order, code below fix it
-	new_shape = [data.shape[i] for i in [0,2,1]]
-	data = data.reshape(new_shape)
-
-if arg.scan_continuous:
-	path = loadpath()
-	position(path[0],daq)
-	daq.configure_timing_sample_clock(**daq_config['scanContinuous'])
-	daq.write(path)
-	park(daq)
-
-if arg.resample:
-	if len(data.shape) == 3:
-		data = data[0,:,:]
-	data = resample(data,config['resample_poly_coef'])
-	x = None 
-
-if arg.fft:
-	data = abs(np.fft.fft(data))
-
-if arg.plot:
-	if len(data.shape) == 3:
-		data = data[0,0,:]
-		x = None
-	if len(data.shape) == 2:
-		data = data[0]
-		x = None
-	if x is None:
-		x = np.arange(len(data))
-	import matplotlib.pyplot as plt
-	plt.plot(x,data)
-	plt.show()
-
-if arg.img_plot:
-	if len(data.shape) == 3:
-		data = data[0]
-	if len(data.shape) == 2:
-		data = data
-	import matplotlib.pyplot as plt
-	plt.imshow(data)
-	plt.show()
-
-if arg.store:
-	filename = "data.dat"
-	np.savetxt(filename,data)
