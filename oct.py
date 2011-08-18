f0d05f64cb171c2ae5334cf76d23c81481ff0df6
Bernardo Kyotoku <bernardo@kyotoku.org>
2011-08-17 22:49:53 -0300

    flags

--- oct.py
+++ oct.py
@@ -4,32 +4,32 @@ import logging
 from configobj import ConfigObj
 from validate import Validator
 
+flags = [
+	'calibrate-spectrum',
+	'x',
+	'get-p',
+	'resample-d',
+	'fft',
+	'non-cor-fft',
+	'line',
+	'scan-3D',
+	'scan-continuous',
+	'scan-single',
+	'load',
+	'store',
+	'plot',
+	'img-plot',
+	]
+
 def parse():
 	import argparse
 	parser = argparse.ArgumentParser()
 	parser.description = "OCT client."
-	flags = [
-		'horz-cal',
-		'plot',
-		'zimg-plot',
-		'store',
-		'x',
-		'get-p',
-		'resample-d',
-		'fft',
-		'non-cor-fft',
-		'line',
-		'scan-3D',
-		'scan-continuous',
-		'scan',
-		'load',
-		]
 	for flag in flags:
 		parser.add_argument('--' + flag,action='store_true') 
 	return parser.parse_args()
 
 arg = parse()
-args = filter(lambda element: not element.startswith('_'),dir(arg))
 validator = Validator({'log':function.log_type,'float':float})
 config = ConfigObj('config.ini',configspec='configspec.ini')
 if not config.validate(validator):
@@ -37,8 +37,8 @@ if not config.validate(validator):
 logging.basicConfig(filename='oct.log',level=config['log'])
 
 data = []
-for i in args:
-	if getattr(arg,i):
+for i in flags:
+	if getattr(arg,i.replace('-','_')):
 		print i
 		fun = getattr(function,i)
 		data = fun(config,data)	
