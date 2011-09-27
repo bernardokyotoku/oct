612d7fe6f8b9b349348030a02ab085f43bff0a64
Bernardo Kyotoku <bernardo@kyotoku.org>
2011-09-27 01:55:41 -0300

    add hardware control

---
+++ hardware_control.py
@@ -0,0 +1,12 @@
+def set_voltage_to_channel(channel,voltage):
+	from nidaqmx import AnalogOutputTask
+	tolerance = 1
+	laserTask = AnalogOutputTask()
+	laserTask.create_voltage_channel(channel,min_val=voltage - tolerance,
+			max_val=voltage + tolerance)
+	laserTask.write(voltage)
+	laserTask.clear()
+
+def turn_laser(state):
+	voltage = 5 if state == "on" else 0
+	set_voltage_to_channel("Dev1/ao2",voltage)
