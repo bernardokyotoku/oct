62acbb7c82443ed6f6dec16b48a2132769ba309a
Bernardo Kyotoku <bernardo@kyotoku.org>
2011-09-30 21:40:39 -0300

    refactor processor

--- processor.py
+++ processor.py
@@ -8,36 +8,10 @@ from configobj import ConfigObj
 from validate import Validator
 from function import transform,log_type,resample
 
-flags = [ ] 
 
-validator = Validator({'log':log_type,'float':float})
-config = ConfigObj('config.ini',configspec='configspec.ini')
-def parse():
-	import argparse
-	parser = argparse.ArgumentParser()
-	parser.description = "OCT client."
-	parser.add_argument('-i',dest='in_file', default=config['in_file'])
-	parser.add_argument('-o',dest='out_file', default=config['out_file'])
-	for flag in flags:
-		parser.add_argument('--' + flag,action='store_true') 
-	return parser.parse_args()
-
-arg = parse()
-if not config.validate(validator):
-	raise Exception('config.ini does not validate with configspec.ini.')
-logging.basicConfig(filename='oct.log',level=config['log'])
-
-for i in flags:
-	if getattr(arg,i.replace('-','_')):
-		fun = getattr(function,i)
-		data = fun(config,data)	
-
-in_file = arg.in_file if arg.in_file else config['in_file']
-out_file = arg.out_file if arg.out_file else config['out_file']
-
-while True:
-	in_fd = open(in_file)
-	out_fd = open(out_file,'w',0)
+def main(config,arg):
+	in_fd = open(arg.in_file)
+	out_fd = open(arg.out_file,'w',0)
 	while True:
 		try:
 			data = cPickle.load(in_fd)
@@ -54,3 +28,33 @@ while True:
 			break
 	in_fd.close()
 	out_fd.close()
+	return arg.daemon
+
+def parse_config():
+	validator = Validator({'log':log_type,'float':float})
+	config = ConfigObj('config.ini',configspec='configspec.ini')
+	if not config.validate(validator):
+		raise Exception('config.ini does not validate with configspec.ini.')
+	return config
+
+def parse_arguments():
+	import argparse
+	flags = ["daemon"]
+	parser = argparse.ArgumentParser()
+	parser.description = "OCT client."
+	parser.add_argument('-i',dest='in_file', default=config['in_file'])
+	parser.add_argument('-o',dest='out_file', default=config['out_file'])
+	for flag in flags:
+		parser.add_argument('--' + flag,action='store_true',default=False) 
+	return parser.parse_args()
+
+config = parse_config()
+arg = parse_arguments()
+logging.basicConfig(filename='oct.log',level=config['log'])
+
+while __name__ == "__main__":
+	repeat = main(config,arg)
+	if not repeat:
+		break
+	 
+	
