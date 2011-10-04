7490956bf8cd38792ee9761f60173d537f9fcaa8
Bernardo Kyotoku <bernardo@kyotoku.org>
2011-10-04 08:12:46 -0300

    adjust methods

--- processor.py
+++ processor.py
@@ -5,38 +5,49 @@ import logging
 import sys
 import cPickle
 import Image
+import os
 from configobj import ConfigObj
 from validate import Validator
 from function import transform,log_type,resample
-
+import simplejson as json
 
 def main(config,arg):
-	in_fd = open(arg.in_file)
-	out_fd = open(arg.out_file,'w',0)
+	source = open(arg.in_file)
+	sink = open(arg.out_file,'w',0)
+	adjust = open_nonblock(arg.adjust_file)
+	adjust_param = config["adjust"]
+
 	while True:
-		try:
-			data = cPickle.load(in_fd)
-		except Exception:
-			break
-		parameters = {"brightness":-100,"contrast":1}
+		try: data = cPickle.load(source)
+		except Exception: break
+		adjust_param = update_adjust(adjust_param,adjust)
 		data = resample(data,config)
 		data = transform(data.T).T
 		data = 10*np.log(data)
-		data = renormalize(data,parameters)
+		data = renormalize(data,adjust_param)
 		data = np.ascontiguousarray(np.uint8(data))
 		image = Image.frombuffer("L",data.shape,data=data.data)
-		try:
-			image.save(out_fd,format='jpeg')
-		except Exception:
-			break
-	in_fd.close()
-	out_fd.close()
+		try: image.save(sink,format='jpeg')
+		except Exception: break
+
+	source.close()
+	sink.close()
 	return arg.daemon
 
 def renormalize(data,parameters):
 	data = data + parameters["brightness"]
 	data = data * parameters["contrast"]
 	return data
+
+def open_nonblock(filename):
+	fd = os.open(filename,os.O_RDONLY|os.O_NONBLOCK)
+	return os.fdopen(fd)
+
+def update_adjust(old_param,adjust):
+	new_param = old_param 
+	try: new_param = json.load(adjust) 
+	except: pass
+	return new_param
 
 def parse_config():
 	validator = Validator({'log':log_type,'float':float})
@@ -64,5 +75,3 @@ while __name__ == "__main__":
 	repeat = main(config,arg)
 	if not repeat:
 		break
-	 
-	
