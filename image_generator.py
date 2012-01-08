8a5e0b40fabc83a4954caf28299644c671948d46
Bernardo Kyotoku <bernardo@kyotoku.org>
2012-01-08 18:23:23 -0200

    a lot of changes

--- image_generator.py
+++ image_generator.py
@@ -4,58 +4,71 @@ from cPickle import dump, HIGHEST_PROTOCOL
 from time import sleep
 import Image
 
-flags=['d']
 def parse():
 	import argparse
 	parser = argparse.ArgumentParser()
 	parser.description = "Raw data simulator."
-	parser.add_argument('-o',dest='out_file', default='raw_data.jpg')
-	parser.add_argument('-count',dest='count', default=1000000000)
-	parser.add_argument('-height',dest='height', default=240)
-	parser.add_argument('-width',dest='width', default=320)
-
-	for flag in flags:
-		parser.add_argument('-' + flag, action='store_true') 
+	parser.add_argument('-o',dest='out_file', default='raw_data.bmp')
+	parser.add_argument('--count',dest='count', default=1000000000)
+	parser.add_argument('--height',dest='height', default=240)
+	parser.add_argument('--width',dest='width', default=320)
+	parser.add_argument('-',dest='stdout', action='store_true')
+	parser.add_argument('-d', action='store_true') 
+	parser.add_argument('-n', action='store_true') 
 	return parser.parse_args()
 arg = parse()
 
-def gauss(x,x0,d):
-    return np.exp(-((x-x0)/d)**2)
+def matrix(phase):
+	def gauss(x,x0,d):
+		return np.exp(-((x-x0)/d)**2)
+	def line(x):
+		carrier = np.sin(0.1*x*np.linspace(0,1000,arg.width))
+		envelope = gauss(np.linspace(0,10,arg.width),5,1)
+		return envelope*carrier
+	frequencies = 2*np.sin(phase + 2*np.linspace(0,2,arg.height)) + 3
+	return np.vstack([line(f) for f in frequencies])
 
-line = lambda x:gauss(np.linspace(0,10,arg.width),5,1)*np.sin(0.1*x*np.linspace(0,1000,arg.width))
-
-image = lambda y: np.vstack([line(x) for x in 2*np.sin(y + 2*np.linspace(0,2,arg.height)) + 3])
-i=0
 print arg.out_file
 
 def scale(image,factor):
 	return np.float32(image)*factor
 
 def getType(filename):
-	return filename.split('.')[1]
+	return filename.split('.')[1].upper()
 
 filetype = getType(arg.out_file)
-
-while True:
-	fd = open(arg.out_file,'w',0)
-	while True:
+def main():
+	i = 0
+	unbuffered = 0
+	fd = open(arg.out_file,'w+b',unbuffered)
+	while arg.n:
 		i += 1
-		print i
-		m = scale(image(i*0.1),factor=255)
-		here = Image.frombuffer(mode = 'F',size=m.T.shape, data=m.data)
-		here = here.convert('L')
+		moving_factor = 0.1
+		print "Creating image ", i, "."
+		m = scale(matrix(i*moving_factor),factor=255)
+		m = np.uint8(m)
+		mode = "L"
+		size = m.T.shape
+		data = m.data
+		image = Image.frombuffer(mode,size, data,"raw",mode,0,1)
 		#plt.figure()
-		#plt.imshow(here)
+		#plt.imshow(image)
 		#plt.show()
 		try:
-			here.save(fd,filetype)
+			image.save(fd,filetype)
 		except Exception, e:
-			print e 
+			print "Error", e.message
 			break
 		#dump(image(t),fd)
 		sleep(0.05)
 		if (i >= int(arg.count)):
-			break
+			print "exit"
+			return True
 	fd.close()
 	if not arg.d:
-		print "Was braken"
+		return True
+
+if __name__ == "__main__":
+	exit = False
+	while not exit or arg.n:
+		exit = main()
