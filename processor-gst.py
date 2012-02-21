b62646646da9829930670021025c7cbda0e23af5
Bernardo Kyotoku <bernardo@kyotoku.org>
2012-02-21 00:19:15 -0200

    changed 8 spaces tab for 4

--- processor-gst.py
+++ processor-gst.py
@@ -37,79 +37,83 @@ _log.setLevel(logging.DEBUG)
 
 
 class Processor(object):
-	'''
-	'''
+    '''
+    '''
 
-	def __init__(self, in_file, out_file, **kwargs):
-		self.loop = gobject.MainLoop()
-		self.pickle = cPickle.Unpickler(open(arg.in_file))
-		self.out_fd = open(arg.out_file,'w',0)
-		if kwargs.get('src'):
-			self.source_file = kwargs.get('src')
-		self.setup_gstreamer()
+    def __init__(self, args, **kwargs):
+        self.loop = gobject.MainLoop()
+        self.args = args
+        self.pickle = cPickle.Unpickler(open(args.in_file))
+        if kwargs.get('src'):
+            self.source_file = kwargs.get('src')
+        self.setup_gstreamer()
 
-	def run(self):
-		self.pipeline.set_state(gst.STATE_PLAYING)
-		self.loop.run()
+    def run(self):
+        self.pipeline.set_state(gst.STATE_PLAYING)
+        self.loop.run()
 
-	def stop(self):
-		self.loop.quit()
+    def stop(self):
+        self.loop.quit()
 
-	def renormalize(self,data,parameters):
-		data = data + parameters['brightness']
-		data = data * parameters['contrast']
-		return data
+    def renormalize(self,data,parameters):
+        data = data + parameters['brightness']
+        data = data * parameters['contrast']
+        return data
 
-	def needdata(self, src, length):
-		try:
-			data = self.pickle.load()
-		except Exception:
-			sys.stderr.write("could not pickle")
-			return
-		parameters = {"brightness":-00,"contrast":8}
-		#data = np.linspace(0,240,320*240).reshape((320,240))
-	#	data = resample(data.T,config)
-		data = transform(data)
-		#data = 10*np.log(data)
-		data = self.renormalize(data, parameters)
-		data = np.ascontiguousarray(np.uint8(data))
-		src.emit('push-buffer', gst.Buffer(data.T.data))
+    def needdata(self, src, length):
+        try:
+            data = self.pickle.load()
+        except Exception:
+            sys.stderr.write("could not pickle")
+            return
+        parameters = {"brightness":-00,"contrast":8}
+        #data = np.linspace(0,240,320*240).reshape((320,240))
+    #    data = resample(data.T,config)
+        data = transform(data)
+        #data = 10*np.log(data)
+        data = self.renormalize(data, parameters)
+        data = np.ascontiguousarray(np.uint8(data))
+        src.emit('push-buffer', gst.Buffer(data.T.data))
 
-	def setup_gstreamer(self):
-		self.pipeline = gst.Pipeline('pipeline')
-		source = gst.element_factory_make('appsrc', 'source')
-		caps = gst.Caps('video/x-raw-gray, bpp=8, endianness=1234, width=320, height=240, framerate=(fraction)1/10')
-		source.set_property('caps', caps)
-		source.set_property('blocksize', 320*240*1)
-		source.connect('need-data', self.needdata)
-		colorspace = gst.element_factory_make('ffmpegcolorspace')
-		caps = gst.Caps('video/x-raw-yuv, width=320, height=240, framerate=(fraction)1/10, format=(fourcc)Y8')
-		videosink = gst.element_factory_make('xvimagesink')
-		videosink.caps = caps
-		self.pipeline.add(source, colorspace, videosink)
-		gst.element_link_many(source, colorspace, videosink)
-		return self.pipeline
+    def setup_gstreamer(self):
+        self.pipeline = gst.Pipeline('pipeline')
+        source = gst.element_factory_make('appsrc', 'source')
+        caps = gst.Caps('video/x-raw-gray, bpp=8, endianness=1234, width=320, height=240, framerate=(fraction)1/10')
+        source.set_property('caps', caps)
+        source.set_property('blocksize', 320*240*1)
+        source.connect('need-data', self.needdata)
+        colorspace = gst.element_factory_make('ffmpegcolorspace')
+        caps = gst.Caps('video/x-raw-yuv, width=320, height=240, framerate=(fraction)1/10, format=(fourcc)Y8')
+        if self.args.out_file is not None:
+            sink = gst.element_factory_make("filesink")
+            sink.set_property("location",self.args.out_file)
+        else:
+            sink = gst.element_factory_make('xvimagesink')
+        sink.caps = caps
+        self.pipeline.add(source, colorspace, sink)
+        gst.element_link_many(source, colorspace, sink)
+        return self.pipeline
 
 def parse_arguments():
-	flags = ['daemon']
-	parser = argparse.ArgumentParser()
-	parser.description = 'OCT client.'
-	parser.add_argument('-i',dest='in_file', default=config['in_file'])
-	parser.add_argument('-o',dest='out_file', default=config['out_file'])
-	for flag in flags:
-		parser.add_argument('--' + flag,action='store_true',default=False) 
-	return parser.parse_args()
+    flags = ['daemon']
+    parser = argparse.ArgumentParser()
+    parser.description = 'OCT client.'
+    parser.add_argument('-i',dest='in_file', default=config['in_file'])
+    parser.add_argument('-o',dest='out_file')
+    for flag in flags:
+        parser.add_argument('--' + flag,action='store_true',default=False) 
+    return parser.parse_args()
 
 def parse_config():
-	validator = Validator({'log':log_type,'float':float})
-	config = ConfigObj('config.ini', configspec='configspec.ini')
-	if not config.validate(validator):
-		raise Exception('config.ini does not validate with configspec.ini.')
-	return config
+    validator = Validator({'log':log_type,'float':float})
+    config = ConfigObj('config.ini', configspec='configspec.ini')
+    if not config.validate(validator):
+        raise Exception('config.ini does not validate with configspec.ini.')
+    return config
 
 if __name__ == '__main__':
-	config = parse_config()
-	arg = parse_arguments()
-	logging.basicConfig(filename='oct.log',level=config['log'])
-	player = Processor(arg.in_file,arg.out_file)
-	player.run()
+    config = parse_config()
+    args = parse_arguments()
+    logging.basicConfig(filename='oct.log',level=config['log'])
+    player = Processor(args)
+    player.run()
