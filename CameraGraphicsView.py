1508fdaaa08bab82f7b2980806c4aaebd61ca118
Bernardo Kyotoku <bernardo@kyotoku.org>
2012-06-08 19:34:43 -0300

    add CameraGraphicsView file

--- CameraGraphicsView.py
+++ CameraGraphicsView.py
@@ -6,5 +6,12 @@ class CameraGraphicsView(QtGui.QGraphicsView):
         
     def sizeHint(self):
         size = self.size()
-        size.setWidth(size.height()*4/3)
+        size.setWidth(size.height()*4./3.)
         return size
+
+    def closeEvent(self, event):
+        print "closing"
+        self.camera_timer.stop()
+        self.camera_device.StopLiveVideo()
+        self.camera_device.FreeImageMem()
+        self.camera_device.ExitCamera()
