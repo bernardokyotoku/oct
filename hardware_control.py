43149327b3d39d6bd892e9364636563af3a47a5e
Bernardo Kyotoku <bernardo@kyotoku.org>
2012-07-07 18:54:13 -0300

    expanded tab in hardware_control

--- hardware_control.py
+++ hardware_control.py
@@ -1,12 +1,13 @@
+import sys
 def set_voltage_to_channel(channel,voltage):
-	from nidaqmx import AnalogOutputTask
-	tolerance = 1
-	laserTask = AnalogOutputTask()
-	laserTask.create_voltage_channel(channel,min_val=voltage - tolerance,
-			max_val=voltage + tolerance)
-	laserTask.write(voltage)
-	laserTask.clear()
+    from nidaqmx import AnalogOutputTask
+    tolerance = 1
+    laserTask = AnalogOutputTask("pointer")
+    laserTask.create_voltage_channel(channel,min_val=voltage - tolerance,
+            max_val=voltage + tolerance)
+    laserTask.write(voltage)
+    laserTask.clear()
 
 def turn_laser(state):
-	voltage = 3.5 if state == "on" else 0
-	set_voltage_to_channel("Dev1/ao3",voltage)
+    voltage = 3.5 if state == "on" else 0
+    set_voltage_to_channel("Dev1/ao3",voltage)
